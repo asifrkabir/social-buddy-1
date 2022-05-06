0e91232ae45f9239ea9a5b03f698c26791a5bc73
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-05-06 17:52:14 +0600

    useEffect dependency issue resolved

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -14,14 +14,14 @@ const PostDetail = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setPost(data))
-    }, [])
+    }, [id])
 
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`;
         fetch(url)
         .then(res => res.json())
         .then(data => setComments(data))
-    }, [])
+    }, [id])
 
     return (
         <div>
